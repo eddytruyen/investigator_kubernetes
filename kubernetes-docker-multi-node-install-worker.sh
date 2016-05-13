0f9fc01ed1f8b730d56951b8183bb6552dcc9979
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2016-05-13 08:16:34 +0000

    addes re-install script for worker

---
+++ kubernetes-docker-multi-node-install-worker.sh
@@ -0,0 +1,5 @@
+#execute as root
+rm -r /var/lib/docker
+rm -r /var/lib/kubelet
+/home/ubuntu/kubernetes_on_openstack/dockerinstall
+/home/ubuntu/kubernetes/docs/getting-started-guides/docker-multinode/worker.sh
