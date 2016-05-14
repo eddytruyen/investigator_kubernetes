25dbbba5e938693daaca27df2d0dd03251005cf3
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2016-05-14 21:57:49 +0000

    added swarm-manager-install-file

---
+++ swarm-installmanager.sh
@@ -0,0 +1,4 @@
+sudo service docker stop
+sudo nohup docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
+docker run -d -p 8500:8500 --name=consul progrium/consul -server -bootstrap
+docker run -d -p ${MASTER_IP}:4000:4000 swarm manage -H :4000 --replication --advertise ${MASTER_IP}:4000 consul://${MASTER_IP}:8500
