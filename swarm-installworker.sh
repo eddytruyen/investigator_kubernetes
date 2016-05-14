cdce0550f0df54bcf8ab68904a9c3b2a3ff8fae9
eddytruyen <Eddy.Truyen@cs.kuleuven.be>
2016-05-14 22:21:32 +0000

    finetuning

--- swarm-installworker.sh
+++ swarm-installworker.sh
@@ -1,4 +1,5 @@
 sudo service docker stop
-sudo nohup docker daemon --label mongodbHost=1 -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
-docker run -d swarm join --advertise=${NODE_IP}:2375 consul://${CONSUL_IP}:8500
+sudo nohup docker daemon --label NodeIP=`/sbin/ip addr show eth0 | awk -F"[ /]+" '/inet / {print $3}'` -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
+sleep 5;
+docker run -d swarm join --advertise=`/sbin/ip addr show eth0 | awk -F"[ /]+" '/inet / {print $3}'`:2375 consul://${CONSUL_IP}:8500
 
